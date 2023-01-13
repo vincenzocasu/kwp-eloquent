1549d034f0c92c58ea76132a9db4750fb7718351
cmachu <pawel.cmachowski@develtio.com>
2023-01-13 12:44:18 +0100

    feat:Add fillable property to Post model

--- src/Model/Post.php
+++ src/Model/Post.php
@@ -22,7 +22,15 @@ class Post extends BaseModel
     
     /** @var string */
     public const UPDATED_AT = 'post_modified';
-    
+
+    /** @var array */
+    protected $fillable = [
+        'ID', 'post_author', 'post_date', 'post_date_gmt', 'post_content', 'post_title', 'post_excerpt', 'post_status',
+        'comment_status', 'ping_status', 'post_password', 'post_name', 'to_ping', 'pinged', 'post_modified',
+        'post_modified_gmt', 'post_content_filtered', 'post_parent', 'guid', 'menu_order', 'post_type',
+        'post_mime_type', 'comment_count'
+    ];
+
     public function newQuery()
     {
         $query = parent::newQuery();
@@ -31,6 +39,7 @@ class Post extends BaseModel
         }
         return $query;
     }
+
 
     /**
      * @return \Illuminate\Database\Eloquent\Collection
